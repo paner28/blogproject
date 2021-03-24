7a34632792243213b1e7c96312dff6d4d465abae
paner <toshimichi23masa@gmail.com>
2021-03-24 19:17:04 +0900

    first commit

---
+++ blogpost/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import BlogList,BlogDetail,BlogCreate,BlogDelete,BlogUpdate,indexview,skillview,sportview,BlogTopic,BlogAnime,BlogMath,BlogFood
+
+urlpatterns = [
+    path('list/', BlogList.as_view(), name='list'),
+    path('index/', indexview, name="index"),
+    path('skill/', skillview, name="skill"),
+    path('anime/', BlogAnime.as_view(), name="anime"),
+    path('sport/', sportview, name="sport"),
+    path('math/', BlogMath.as_view(), name="math"),
+    path('food/', BlogFood.as_view(), name="food"),
+    path('topic/<str:parameter>', BlogTopic.as_view(), name='topic'),
+    path('detail/<int:pk>', BlogDetail.as_view(), name='detail'),
+    path('create/', BlogCreate.as_view(), name='create'),
+    path('delete/<int:pk>', BlogDelete.as_view(), name='delete'),
+    path('update/<int:pk>', BlogUpdate.as_view(), name='update'),
+]
---
+++ blogpost/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
+from .models import BlogModel,AnimeModel,RamennModel
+from django.urls import reverse_lazy
+
+class BlogList(ListView):
+    template_name = 'list.html'
+    model = BlogModel
+
+class BlogDetail(DetailView):
+    template_name = 'detail.html'
+    model = BlogModel
+
+class BlogTopic(ListView):
+    template_name = "topic.html"
+    model = BlogModel
+
+    def get(self, request, *args, **kwargs):
+        parameter = self.kwargs.get('parameter')
+        if parameter:
+            request.session['parameter'] =parameter
+        return super().get(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        parameter = self.request.session.pop('parameter', None)
+        if parameter:
+            context['parameter'] =parameter
+        return context
+
+# def topicview(request):
+#     data = {'topic' :request.GET.get('item')}
+#     return render(request, 'topic.html', data)
+
+class BlogCreate(CreateView):
+    template_name = 'create.html'
+    model = BlogModel
+    fields = ( 'title', 'content', 'category')
+    success_url = reverse_lazy('list')
+
+class BlogDelete(DeleteView):
+    template_name = 'delete.html'
+    model = BlogModel
+    success_url = reverse_lazy('list')
+
+class BlogUpdate(UpdateView):
+    template_name = 'update.html'
+    model = BlogModel
+    fields = ( 'title', 'content', 'category')
+    success_url = reverse_lazy('list')
+
+def indexview(request):
+    return render(request, 'index.html')
+
+def skillview(request):
+    return render(request, 'skill.html')
+
+class BlogAnime(ListView):
+    template_name = 'anime.html'
+    model = AnimeModel
+
+def sportview(request):
+    return render(request, 'sport.html')
+
+class BlogMath(ListView):
+    template_name = 'math.html'
+    model = BlogModel
+
+class BlogFood(ListView):
+    template_name = 'food.html'
+    model = RamennModel
