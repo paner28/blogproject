22a06272bc0549685843664433662f1be2267f48
paner <toshi23masa@gmail.com>
2021-03-25 22:41:35 +0900

    hehe

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,4 +149,4 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
