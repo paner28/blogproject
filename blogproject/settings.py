e7e65f0cb5dc6341469c5cfb0dd4cda02c6930aa
paner <toshi23masa@gmail.com>
2021-03-27 11:23:07 +0900

    jikkenn

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/app/staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
